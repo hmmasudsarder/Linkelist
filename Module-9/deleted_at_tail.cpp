c92c09db96f6bd3a1382e3e880677b52d84a4dc8
hmmasudsarder <hmmasudsardar@gmail.com>
2025-07-03 01:22:47 +0600

    added deleted in tail

---
+++ Module-9/deleted_at_tail.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forwared(Node*  head){
+    Node* temp = head;
+    while (temp != NULL)
+    {
+       cout << temp->val << " ";
+       temp = temp->next ;
+    }
+    cout << endl;
+}
+
+void delete_at_tail(Node* &head, Node* &tail){
+    Node * deletedNode = tail;
+    tail = tail->prev;
+    delete deletedNode;
+    if (tail == NULL)
+    {
+        head = NULL;
+        return;
+    }
+    tail->next = NULL;
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(200);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    delete_at_tail(head, tail);
+    print_forwared(head);
+    return 0;
+}
